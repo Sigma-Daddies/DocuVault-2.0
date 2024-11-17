d10fee43ab8479bd94ae86734eb7e242e236a031
kangnathan <C1206-23@itmlyceumalabang.onmicrosoft.com>
2024-11-18 00:51:20 +0800

    Database connection modification

--- DocuVault/Data/AccessDB.cs
+++ DocuVault/Data/AccessDB.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.OleDb;
 
-namespace DocuVault.Data
+namespace DocuVault
 {
-    internal class AccessDB
+    public class AccessDB
     {
+        private readonly string _connectionString;
+
+        public AccessDB()
+        {
+            // Initialize the connection string
+            _connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Karl\Documents\GitHub\DocuVault-2.0\DocuVault.accdb";
+        }
+
+        public OleDbConnection GetConnection()
+        {
+            return new OleDbConnection(_connectionString);
+        }
+
+        public void Execute(Action<OleDbConnection> action)
+        {
+            using (var connection = GetConnection())
+            {
+                try
+                {
+                    connection.Open();
+                    action(connection);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Database operation failed: " + ex.Message, ex);
+                }
+            }
+        }
+
+        public T Execute<T>(Func<OleDbConnection, T> func)
+        {
+            using (var connection = GetConnection())
+            {
+                try
+                {
+                    connection.Open();
+                    return func(connection);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Database operation failed: " + ex.Message, ex);
+                }
+            }
+        }
     }
 }
