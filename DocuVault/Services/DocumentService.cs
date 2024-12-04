d31a850176d1e4ef6485b5b445b3caed9cdf7993
kangnathan <C1206-23@itmlyceumalabang.onmicrosoft.com>
2024-12-05 01:13:31 +0800

    Create DocumentService.cs

---
+++ DocuVault/Services/DocumentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Windows.Controls;
+using System.Linq;
+using DocuVault.Models;
+using DocuVault;
+
+namespace DocumentManagementSystem.Services
+{
+    public class DocumentService
+    {
+    }
+}
