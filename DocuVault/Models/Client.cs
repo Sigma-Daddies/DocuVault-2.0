6545d69f6d5ca5300f6a5fd459159a903cd2a800
Punzalan-Rayver-Ravano-BSCS21A1 <c1171-23@itmlyceumalabang.onmicrosoft.com>
2024-12-06 14:05:36 +0800

    Added Client.cs

---
+++ DocuVault/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace DocuVault.Models
+{
+    public class Client
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public ICommand LockCommand { get; set; }
+        public ICommand UnlockCommand { get; set; }
+    }
+}
